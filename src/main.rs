f03e9de238e4f830c38198c2a90b93133b9bbcf6
iapposki <konark981999@gmail.com>
2024-05-23 16:09:41 +0530

    constructor for config

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,24 @@ use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+    let config = Config::new(&args);
 
-    let query = &args[1];
-    let file_path = &args[2];
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
 
-    let contents = fs::read_to_string(file_path).expect("Couldn't read the file.");
-    println!("contents of file {contents}");
+    let contents =
+        fs::read_to_string(config.file_path).expect("Shrould have been able to read the file");
+}
+
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+        Config { query, file_path }
+    }
 }
