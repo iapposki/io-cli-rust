35b13e6eaaf1368b1345aa724a3cd5fd75dbb993
iapposki <konark981999@gmail.com>
2024-05-24 14:10:30 +0530

    error handling for config parsing

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,22 @@
 use std::env;
 use std::fs;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config = Config::new(&args);
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1)
+    });
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
 
     let contents =
         fs::read_to_string(config.file_path).expect("Shrould have been able to read the file");
+
+    println!("{contents}");
+    
 }
 
 struct Config {
@@ -18,9 +25,14 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+
+        if args.len() < 3 {
+            // panic!("not enough arguments provided.");
+            return Err("not enough arguments")
+        }
         let query = args[1].clone();
         let file_path = args[2].clone();
-        Config { query, file_path }
+        Ok(Config { query, file_path })
     }
 }
